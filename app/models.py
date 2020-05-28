78c6f194e1a613adf010e3faf2909885c270df19
petherman <vasinpether@gmail.com>
2020-05-28 20:53:05 +1000

    Adding order handing and price calculation

--- app/models.py
+++ app/models.py
@@ -6,12 +6,13 @@ Copyright (c) 2019 - present AppSeed.us
 
 from app         import db
 from flask_login import UserMixin
+from sqlalchemy import *
 
 class User(UserMixin, db.Model):
 
-    id       = db.Column(db.Integer,     primary_key=True)
-    user     = db.Column(db.String(64),  unique = True)
-    email    = db.Column(db.String(120), unique = True)
+    id = db.Column(db.Integer,     primary_key=True)
+    user = db.Column(db.String(64),  unique = True)
+    email = db.Column(db.String(120), unique = True)
     password = db.Column(db.String(500))
 
     def __init__(self, user, email, password):
@@ -29,7 +30,15 @@ class User(UserMixin, db.Model):
         db.session.commit( )
         return self 
 
+
+# This table is a helper table to facilitate many to many relationship
+order_lines = db.Table('order_lines',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('order_item_id', db.Integer, db.ForeignKey('menu_item.id'), primary_key=True)
+)
+
 class MenuItem(db.Model):
+    __tablename__ = 'menu_item'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     description = db.Column(db.String(250))
@@ -47,14 +56,31 @@ class MenuItem(db.Model):
         # commit change and save the object
         db.session.commit( )
         return self 
-#class OrderLine(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    order_number = db.relationship('Order', backref='order_number', lazy='dynamic')
-#    order_item = db.relationship('MenuItem', backref='order_item',lazy='dynamic')
+                
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    table_number = db.Column(db.Integer)
+    #order_items = db.relationship('MenuItem', secondary='order_lines', lazy='subquery',backref=db.backref('orders', lazy=True))
+    order_items = db.relationship("MenuItem", secondary=order_lines)
     
-#class Order(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    table_number = db.Column(db.Integer)
+    def __init__(self,table_number):
+        self.table_number = table_number
+        
+    def add_item(self,item):
+        self.order_items.append(item)
+        # commit change and save the object
+        db.session.commit()
+    
+    def calc_total(self):
+        total=float(0.00)
+        for item in self.order_items:
+            total+=float(item.price)
+        total=round(total,2)
+        total=format(total,'.2f')
+        return(str(total))
+
+
 
 
 
